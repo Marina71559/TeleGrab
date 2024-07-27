5dd3c6bcf70955437e6c0962cc9fda3b02a94ad5
Marina71559 <116583721+Marina71559@users.noreply.github.com>
2024-07-27 15:43:51 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+api_id = 21743414
+api_hash = '6a95c4ea4557575588954de61353df6e'
+bot_token = '6791299355:AAFcTcoqe_5JB01r0glSzF469571fvtxvw0'
+my_id = 506453246
+
+technical_channel_id = 1002178195302
+
+new_link = "http://t.me/"
+new_username = "@"
+
+proxy_url = 'http://ip:port'  #заполнять только если планируете рерайтить текст в режиме модерации с помощью Chat GPT
+openai_api_key = "sk-..."  #заполнять только если планируете рерайтить текст в режиме модерации с помощью Chat GPT
